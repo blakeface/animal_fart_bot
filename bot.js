487389f0e52655ef2580010486371cd1c8329fc6
Blake Netzeband <blake.netzeband@gmail.com>
2016-04-05 18:38:58 -0600

    initial commit

---
+++ bot.js
@@ -0,0 +1,9 @@
+var Twit = require('twit'),
+  dotenv = require('dotenv').config();
+
+var T = new Twit({
+  consumerkey: 'QVNSHWDmNYTnMUnOpdnmWETt3',
+  consumersecret: 'Rpxv2XdC9jDXwdjmp2NrPVGfv87CMSQ6P4WijdfL6YLER6TqRK',
+  accesstoken: '	717508463501320193-B8bIbf2SgCM5Tu6jqujMV7QRgm2mmFE',
+  accesstoken_secret: 'bLPWUr5UVkzqtxva8AYrS7uwD4CroCfWSFeAPuQPJ20BE'
+});
